09d5c5a547e3ceda417777a95b0b3049a6938a69
purpleame <amirireal@gmail.com>
2026-04-27 15:12:00 -0400

    1

--- src/main/java/com/pluralsight/LedgerController.java
+++ src/main/java/com/pluralsight/LedgerController.java
@@ -1,4 +1,15 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class LedgerController {
+
+    public void fileNameGrabber() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the filename to save (e.g., transactions.csv): ");
+        String userFileName = input.nextLine();
+
+        LedgerDataManager manager = new LedgerDataManager();
+        manager.initFile(userFileName);
+    }
 }
--- src/main/java/com/pluralsight/LedgerDataManager.java
+++ src/main/java/com/pluralsight/LedgerDataManager.java
@@ -1,4 +1,29 @@
 package com.pluralsight;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
 public class LedgerDataManager {
+    private String print;
+    private String fileName;
+        FileWriter fileWriter = new FileWriter();
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+    // getters
+    public String getFileName() {return fileName;}
+    // setters
+    public void setFileName(String fileName) {this.fileName = fileName;}
+
+    // methods
+    public void initFile(String userFileName) {
+        try {
+            FileReader fileReader = new FileReader("src\\main\\resources\\" + userFileName);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
